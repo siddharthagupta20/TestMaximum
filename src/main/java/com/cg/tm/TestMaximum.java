09724f994553b1b7131ac01cd831b5793049f18e
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-10-08 22:41:07 +0530

    [Siddhartha] modified: testMax method with 3 test cases

--- src/main/java/com/cg/tm/TestMaximum.java
+++ src/main/java/com/cg/tm/TestMaximum.java
@@ -1,7 +1,17 @@
 package com.cg.tm;
 
 public class TestMaximum {
-	
+
+	public Integer testMax(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (max.compareTo(b) < 0)
+			max = b;
+		if (max.compareTo(c) < 0)
+			max = c;
+		return max;
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Test Maximum Program.");
 	}
--- src/test/java/com/cg/tm/TesterMaximum.java
+++ src/test/java/com/cg/tm/TesterMaximum.java
@@ -1,5 +1,28 @@
 package com.cg.tm;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class TesterMaximum {
+	
+	@Test
+	public void givenFirst_whenMax_returnsFirst() {
+		TestMaximum tm=new TestMaximum();
+		Integer result=tm.testMax(5, 4, 3);
+		assertEquals(new Integer(5), result);
+	}
+	@Test
+	public void givenSecomd_whenMax_returnsSecond() {
+		TestMaximum tm=new TestMaximum();
+		Integer result=tm.testMax(4, 5, 3);
+		assertEquals(new Integer(5), result);
+	}
+	@Test
+	public void givenThird_whenMax_returnsThird() {
+		TestMaximum tm=new TestMaximum();
+		Integer result=tm.testMax(3, 4, 5);
+		assertEquals(new Integer(5), result);
+	}
 
 }
