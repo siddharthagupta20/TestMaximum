e727f3348c6d323b0d0333a938bd0035bf8e489f
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-10-08 23:03:15 +0530

    [Siddhartha] modified: checking max of Float type

--- src/main/java/com/cg/tm/TestMaximum.java
+++ src/main/java/com/cg/tm/TestMaximum.java
@@ -11,6 +11,15 @@ public class TestMaximum {
 		return max;
 
 	}
+	public Float testMaxFloat(Float a, Float b, Float c) {
+		Float max = a;
+		if (max.compareTo(b) < 0)
+			max = b;
+		if (max.compareTo(c) < 0)
+			max = c;
+		return max;
+
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Test Maximum Program.");
--- src/test/java/com/cg/tm/TesterMaximum.java
+++ src/test/java/com/cg/tm/TesterMaximum.java
@@ -9,20 +9,20 @@ public class TesterMaximum {
 	@Test
 	public void givenFirst_whenMax_returnsFirst() {
 		TestMaximum tm=new TestMaximum();
-		Integer result=tm.testMax(5, 4, 3);
-		assertEquals(new Integer(5), result);
+		Float result=tm.testMaxFloat(5.3f, 4.3f, 3.3f);
+		assertEquals(new Float(5.3), result);
 	}
 	@Test
 	public void givenSecomd_whenMax_returnsSecond() {
 		TestMaximum tm=new TestMaximum();
-		Integer result=tm.testMax(4, 5, 3);
-		assertEquals(new Integer(5), result);
+		Float result=tm.testMaxFloat(4.3f, 5.3f, 3.3f);
+		assertEquals(new Float(5.3), result);
 	}
 	@Test
 	public void givenThird_whenMax_returnsThird() {
 		TestMaximum tm=new TestMaximum();
-		Integer result=tm.testMax(3, 4, 5);
-		assertEquals(new Integer(5), result);
+		Float result=tm.testMaxFloat(3.3f, 4.3f, 5.3f);
+		assertEquals(new Float(5.3), result);
 	}
 
 }
