2ec52074152be9b843d838a39c9c771be61c8ff0
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-10-08 23:10:28 +0530

    [Siddhartha] modified: checking max of String type

--- src/test/java/com/cg/tm/TesterMaximum.java
+++ src/test/java/com/cg/tm/TesterMaximum.java
@@ -5,24 +5,26 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TesterMaximum {
-	
+
 	@Test
 	public void givenFirst_whenMax_returnsFirst() {
-		TestMaximum tm=new TestMaximum();
-		Float result=tm.testMaxFloat(5.3f, 4.3f, 3.3f);
-		assertEquals(new Float(5.3), result);
+		TestMaximum tm = new TestMaximum();
+		String result = tm.testMaxString("Peach", "Apple", "Banana");
+		assertEquals("Peach", result);
 	}
+
 	@Test
 	public void givenSecomd_whenMax_returnsSecond() {
-		TestMaximum tm=new TestMaximum();
-		Float result=tm.testMaxFloat(4.3f, 5.3f, 3.3f);
-		assertEquals(new Float(5.3), result);
+		TestMaximum tm = new TestMaximum();
+		String result = tm.testMaxString("Apple", "Peach", "Banana");
+		assertEquals("Peach", result);
 	}
+
 	@Test
 	public void givenThird_whenMax_returnsThird() {
-		TestMaximum tm=new TestMaximum();
-		Float result=tm.testMaxFloat(3.3f, 4.3f, 5.3f);
-		assertEquals(new Float(5.3), result);
+		TestMaximum tm = new TestMaximum();
+		String result = tm.testMaxString("Apple", "Banana", "Peach");
+		assertEquals("Peach", result);
 	}
 
 }
